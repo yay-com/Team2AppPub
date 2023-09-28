bc72f054e3e71b87c4c87041322d40ace228c1fd
PodkopovP <113348735+PodkopovP@users.noreply.github.com>
2023-09-28 09:04:15 +0100

    Conf

---
+++ src/app/layout.tsx
@@ -0,0 +1,12 @@
+export const metadata = {
+  title: "Team",
+  description: "By Platon",
+}
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  )
+}
