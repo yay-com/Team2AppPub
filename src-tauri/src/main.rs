222447f6cb1bc5a38912b846544010fe458e4479
PodkopovP <113348735+PodkopovP@users.noreply.github.com>
2023-10-02 13:33:09 +0100

    Background blur

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,7 +3,9 @@
     windows_subsystem = "windows"
 )]
 
+use tauri::Manager;
 use std::time::{SystemTime, UNIX_EPOCH};
+use window_vibrancy::{apply_blur, apply_vibrancy, NSVisualEffectMaterial};
 
 #[tauri::command]
 fn on_button_clicked() -> String {
@@ -17,6 +19,19 @@ fn on_button_clicked() -> String {
 
 fn main() {
     tauri::Builder::default()
+        .setup(|app| {
+              let window = app.get_window("main").unwrap();
+
+              #[cfg(target_os = "macos")]
+              apply_vibrancy(&window, NSVisualEffectMaterial::HudWindow, None, None)
+                .expect("Unsupported platform! 'apply_vibrancy' is only supported on macOS");
+
+              #[cfg(target_os = "windows")]
+              apply_blur(&window, Some((18, 18, 18, 125)))
+                .expect("Unsupported platform! 'apply_blur' is only supported on Windows");
+
+              Ok(())
+            })
         .invoke_handler(tauri::generate_handler![on_button_clicked])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
